4aeb1799d31628408bab51875787b0800c98e6cb
unknown <Administrador@Win-10-02.contactpoint.com.br>
2022-06-18 00:27:57 +0000

    Finalizado

---
+++ person.js
@@ -0,0 +1,12 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+
+    sayMayName() {
+        return `My name is ${this.name}!`;
+    }
+}
+module.exports = {
+    Person,
+}
